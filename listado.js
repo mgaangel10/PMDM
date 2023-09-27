6d1cb290ced993fe6e14f31604354025f2179fb2
mgaangel10 <mg.aangel10@gmail.com>
2023-09-27 22:04:53 +0200

    para ti robe

--- listado.js
+++ listado.js
@@ -1,4 +1,5 @@
 console.log('hola');
+
 $(document).ready(function () {
     $.ajax({
         url: 'https://swapi.dev/api/people',
@@ -6,46 +7,54 @@ $(document).ready(function () {
     }).done(function (resp) {
         var listadoPomemon = resp.results;
         var indice = 1;
+        var id=1;
         listadoPomemon.forEach(function (characters) {
             
             
             // var template = '<p><h1>' + pokemon.name + '</h1></p>';
-            var template = ` <div class="card bg-black text-white border border-white m-2 " style="width: 19rem;">
+            var template = ` <div class="card bg-black text-white border border-warning-subtle  " id="pinchar" style="width: 19rem;">
             <div class="img">
                 <img src="https://starwars-visualguide.com/assets/img/characters/${indice}.jpg" class="card-img-top rounded-4" alt="..."  >
             </div>
-            <div class=" card card-body bg-black text-white" >
+            <div class=" card card-body bg-black text-white card d-flex align-items-center justify-content-center" >
                 <p class="card-text text-center "> <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <ion-icon name="help-circle-outline"></ion-icon>
-                      </button>
+                    <button type="button" class="btn btn-primary " data-bs-toggle="modal" id="btn" data-bs-target="#exampleModal"  personajeid="${id}">
+                    ${characters.name}
+                      </button></p>
                       
-                      <!-- Modal -->
-                      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h1 class="modal-title fs-5 text-black" id="exampleModalLabel">${characters.name}</h1>
-                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                              ...
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><ion-icon name="arrow-undo-outline"></ion-icon></button>
-                              <button type="button" class="btn btn-primary"><ion-icon name="checkmark-done-outline"></ion-icon></button>
-                            </div>
-                          </div>
-                        </div>
-                      </div></p>
             
           </div>
           </div>
         `;
+        id++;
         indice++;
             $('#lista-pokemon').append(template);
             
         });
+        
     });
+    $(document).on('click','#btn',function(){
+      var idPersonaje = $(this).attr('personajeid');
+      
+     
+      $.ajax({
+        url: 'https://swapi.dev/api/people/'+idPersonaje,
+        type: 'GET'
 
-});
+      }).done(function(respuesta){    
+
+        $('#mod').html(respuesta.name);
+        $('#films').html(respuesta.gender);
+        $('#people').html(respuesta.skin_color);
+        $('#vehicules').html(respuesta.eye_color);
+
+          
+      
+        
+          
+          
+       
+       
+      })
+    })
+   });
