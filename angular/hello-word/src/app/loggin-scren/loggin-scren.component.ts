465a3bd0b72ecbf0db0bce518a376e36b02b07e7
mgaangel10 <mg.aangel10@gmail.com>
2023-10-11 14:20:12 +0200

    primer ejercicio

---
+++ angular/hello-word/src/app/loggin-scren/loggin-scren.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loggin-scren',
+  templateUrl: './loggin-scren.component.html',
+  styleUrls: ['./loggin-scren.component.css']
+})
+export class LogginScrenComponent {
+
+}
---
+++ typescript/js.js
@@ -0,0 +1,11 @@
+function bucle(x) {
+    x=100;
+   
+    for (let index = 0; index < x.length; index++) {
+           console.log(x[index]);
+        
+    }
+    return x[index]
+    
+}
+console.log(bucle(x));
