72d003a731af5c42ed86feca485a49e72eb08966
mgaangel10 <mg.aangel10@gmail.com>
2023-10-18 14:30:05 +0200

    solo envar

--- angular/vehiculeapi/src/app/app.module.ts
+++ angular/vehiculeapi/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './home-screen-component/toolbar/toolbar.component';
+import { VehiculosComponent } from './vehiculos/vehiculos.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeScreenComponentComponent } from './home-screen-component/home-screen-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
-    HomeScreenComponentComponent
+    VehiculosComponent
   ],
   imports: [
     BrowserModule,
