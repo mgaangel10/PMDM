beee847b57b81896d5f52632ff4f8f0d60547cf6
mgaangel10 <mg.aangel10@gmail.com>
2024-01-29 13:32:40 +0100

    hacer en casa peticion login the movie db

---
+++ flutter/autentication_flutter/lib/widgtes/widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BotonPerfil extends StatelessWidget {
+  final String token =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OWYzZTZhNTI0MGRiNGVkYWE2M2RmODliMDdkZGM1OSIsInN1YiI6IjY1NDM3YjM1ZWQyYWMyMDBjNjQzNTdiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r4jeUSXd1hlTp-YhvCU8A3UOsKFdXXHssaAC5L2q5mM";
+
+  const BotonPerfil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
